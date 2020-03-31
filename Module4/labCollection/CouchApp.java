0d32173d06f773124bf73716e24496d3451d9771
Chad Zuniga <chad.zuniga10@gmail.com>
2020-03-31 13:36:50 -0600

    Updated foreach loops to lambda expressions

--- Module4/labCollection/CouchApp.java
+++ Module4/labCollection/CouchApp.java
@@ -7,28 +7,28 @@ import java.util.Set;
 
 /**
  * Test client for Couch Class.
- * 
+ *
  * @author Chad Z
  */
 public class CouchApp {
 
 	public static void main(String[] args) {
-		Color[] colors = {Color.BLACK, Color.BLUE, Color.DARK_GRAY, Color.GRAY, 
+		Color[] colors = {Color.BLACK, Color.BLUE, Color.DARK_GRAY, Color.GRAY,
 				Color.GREEN, Color.LIGHT_GRAY, Color.RED, Color.WHITE};
 		ArrayList<Couch> couches = new ArrayList<>();
 		ArrayList<Couch> favorites = new ArrayList<>();
 		addCouches(couches,colors);
 		addFavorites(favorites);
 		printCouchListData(couches,favorites);
-		
+
 		System.out.println("Removing all vinyl couches... \n");
 		couches.removeIf(x -> x.getMaterial() == Material.VINYL);
 		printCouchListData(couches,favorites);
-		
+
 		System.out.println("Adding all the favorites to the list . . .\n");
 		couches.addAll(favorites);
 		printCouchListData(couches,favorites);
-		
+
 		System.out.println("Adding all the couches to a set. . .\n");
 		Set<Couch> couchSet = new HashSet<Couch>(couches);
 		printCouchSetData(couchSet,favorites);
@@ -41,31 +41,27 @@ public class CouchApp {
 			couches.add(new Couch(colors[i],Material.VINYL));
 		}
 	}
-	
+
 	private static void addFavorites(ArrayList<Couch> favorites) {
 		favorites.add(new Couch(Color.RED,Material.FABRIC));
 		favorites.add(new Couch(Color.BLACK,Material.LEATHER));
 		favorites.add(new Couch(Color.WHITE,Material.VINYL));
 	}
-	
+
 	private static void printCouchListData(ArrayList<Couch> couches, ArrayList<Couch> favorites) {
-		for(Couch el:couches) {
-			System.out.println(el);
-		}
+		couches.forEach(el -> System.out.println(el));
 		System.out.printf("Number of elements in list couches: %d%n",couches.size());
 		isFavoritesListIncluded(couches, favorites);
 		System.out.println();
 	}
-	
+
 	private static void printCouchSetData(Set<Couch> couchSet, ArrayList<Couch> favorites) {
-		for(Couch el:couchSet) {
-			System.out.println(el);
-		}
+		couchSet.forEach(el -> System.out.println(el));
 		System.out.printf("Number of elements in the set: %d%n",couchSet.size());
 		isFavoritesSetIncluded(couchSet, favorites);
 		System.out.println("");
 	}
-	
+
 	private static void isFavoritesListIncluded(ArrayList<Couch> couches, ArrayList<Couch> favorites) {
 		if(couches.containsAll(favorites)) {
 			System.out.println("All favorites are in the list.");
@@ -73,7 +69,7 @@ public class CouchApp {
 			System.out.println("Not all favorites are in the list.");
 		}
 	}
-	
+
 	private static void isFavoritesSetIncluded(Set<Couch> couchSet, ArrayList<Couch> favorites) {
 		if(couchSet.containsAll(favorites)) {
 			System.out.println("All favorites are in the set.");
